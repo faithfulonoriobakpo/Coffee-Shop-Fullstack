3e62379e1d40dc6dfa3d4498b2c567e081c7d1fe
faithfulonoriobakpo <faithful.onoriobakpo@gmail.com>
2022-07-28 16:45:52 +0100

    update api audience

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,7 +4,7 @@ export const environment = {
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
     url: 'faithfulonoriobakpo-coffee-shop', // the auth0 domain prefix
-    audience: 'coffee-shop', // the audience set for the auth0 app
+    audience: 'http://localhost:5000', // the audience set for the auth0 app
     clientId: 'jx4wOTPx9IDIK7ftxWSRHHtrRRdiOJQY', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
