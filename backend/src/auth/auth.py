ce523abb599af7eb0093199c3afc958e1c2b3918
faithfulonoriobakpo <faithful.onoriobakpo@gmail.com>
2022-07-28 16:46:50 +0100

    update API_AUDIENCE

--- backend/src/auth/auth.py
+++ backend/src/auth/auth.py
@@ -7,7 +7,7 @@ from urllib.request import urlopen
 
 AUTH0_DOMAIN = 'faithfulonoriobakpo-coffee-shop.us.auth0.com'
 ALGORITHMS = ['RS256']
-API_AUDIENCE = 'coffee-shop'
+API_AUDIENCE = 'http://localhost:5000'
 
 ## AuthError Exception
 '''
