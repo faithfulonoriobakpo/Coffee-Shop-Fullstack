9397450a8a178efc91476f9ab9dbabee3357ccac
faithfulonoriobakpo <faithful.onoriobakpo@gmail.com>
2022-07-30 10:00:51 +0100

    update frontend folder

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,11 +1,14 @@
+/* @TODO replace with your variables
+ * ensure all variables on this page match your project
+ */
 
 export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: 'faithfulonoriobakpo-coffee-shop', // the auth0 domain prefix
-    audience: 'http://localhost:5000', // the audience set for the auth0 app
-    clientId: 'jx4wOTPx9IDIK7ftxWSRHHtrRRdiOJQY', // the client id generated for the auth0 app
+    url: '', // the auth0 domain prefix
+    audience: '', // the audience set for the auth0 app
+    clientId: '', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
 };
